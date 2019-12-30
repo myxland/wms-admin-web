eb74daa9d11da4d46dc35f2d1c44cd118d6bf766
wwywmy <378297869@qq.com>
2019-12-30 14:10:14 +0800

    修改网络架构，由之前转发指向vue项目，变更为vue项目另外部署，转发指向admin项目

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://admin.blogiscapital.cn"'
+  //BASE_API: '"http://admin.blogiscapital.cn"'
+  BASE_API: '"http://wms.free.idcfengye.com"'
 }
